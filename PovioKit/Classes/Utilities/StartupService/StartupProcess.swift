a18e492f3da634f3617f54f60f28f6a1c133e502
Toni Kocjan <toni.kocjan@poviolabs.com>
2018-12-11 10:24:06 +0100

    Podspec

---
+++ PovioKit/Classes/Utilities/StartupService/StartupProcess.swift
@@ -0,0 +1,22 @@
+//
+//  KeychainService.swift
+//  Facelift
+//
+//  Created by Toni Kocjan on 04/07/2018.
+//  Copyright © 2018 Povio Labs. All rights reserved.
+//
+
+import Foundation
+
+///
+/// An abstraction for a predefined set of functionality, aimed to be ran once, at app startup.
+///
+public protocol StartupProcess {
+	func run(completion: @escaping (Bool) -> Void)
+}
+
+extension StartupProcess {
+  public var id: String {
+    return UUID().uuidString
+  }
+}
---
+++ PovioKit/Classes/Utilities/StartupService/StartupProcessService.swift
@@ -0,0 +1,21 @@
+//
+//  StartupProcessService.swift
+//  Facelift
+//
+//  Created by Toni Kocjan on 04/07/2018.
+//  Copyright © 2018 Povio Labs. All rights reserved.
+//
+
+import Foundation
+
+public final class StartupProcessService {
+  @discardableResult
+  public func execute(process: StartupProcess) -> StartupProcessService {
+    process.run { (success) in
+      guard success else { return }
+      print("Successfully ran the \(type(of: process)) process.")
+    }
+    
+    return self
+  }
+}
