2bce8af83b607719fb4bf13aec1ed9268ea03c03
Toni Kocjan <toni.kocjan@poviolabs.com>
2018-12-14 11:27:11 +0100

    Foundation extensions

---
+++ PovioKit/Classes/Extensions/Foundation/Collection-Tasty.swift
@@ -0,0 +1,20 @@
+//
+//  Collection+Extra.swift
+//  PovioKit
+//
+//  Created by Toni Kocjan on 12/03/2018.
+//  Copyright © 2018 Povio Labs. All rights reserved.
+//
+
+import Foundation
+
+public extension Collection {
+	/// Returns the element at the specified index iff it is within bounds, otherwise nil.
+	subscript (safe index: Index) -> Element? {
+		return indices.contains(index) ? self[index]: nil
+	}
+	
+	func count(where clause: (Element) -> Bool) -> Int {
+		return filter(clause).count
+	}
+}
