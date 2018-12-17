fad44f3cbf96e6f7af578c06ca026f0bbfeb9ba9
Toni Kocjan <toni.kocjan@poviolabs.com>
2018-12-17 12:18:31 +0100

    Public initializer

--- PovioKit/Classes/ShareKit/Share/SKSocialMediaPost.swift
+++ PovioKit/Classes/ShareKit/Share/SKSocialMediaPost.swift
@@ -15,4 +15,19 @@ public struct SKSocialMediaPost {
   public let imageUrls: [URL]
   public let url: URL?
   public let videoUrls: [URL]
+  
+  public init(id: String, title: String, content: String, imageUrls: [URL], url: URL?, videoUrls: [URL]) {
+    self.id = id
+    self.title = title
+    self.content = content
+    self.imageUrls = imageUrls
+    self.url = url
+    self.videoUrls = videoUrls
+  }
+  
+  public init(id: String, title: String, content: String, imageUrls: [String], url: String?, videoUrls: [String]) {
+    let imageURLs = imageUrls.compactMap { URL(string: $0) }
+    let videoURLs = videoUrls.compactMap { URL(string: $0) }
+    self.init(id: id, title: title, content: content, imageUrls: imageURLs, url: URL(string: url), videoUrls: videoURLs)
+  }
 }
