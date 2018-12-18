a01898523b9f0e045a42f8db625e864381c6cd59
Toni Kocjan <toni.kocjan@poviolabs.com>
2018-12-18 14:18:36 +0100

    Bumped version

--- PovioKit.podspec
+++ PovioKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'PovioKit'
-  s.version          = '0.1.10'
+  s.version          = '0.1.11'
   s.summary          = 'PovioKit is a collection of useful tools, extensions and other modules.'
   s.swift_version    = '4.2'
 
--- PovioKit/Classes/ShareKit/Social Media/Instagram/SKInstagramBroadcast.swift
+++ PovioKit/Classes/ShareKit/Social Media/Instagram/SKInstagramBroadcast.swift
@@ -19,7 +19,7 @@ class SKInstagramBroadcast: SKSocialMediaBroadcast {
     case noMedia
   }
   
-  let instagramURL: URL = "instagram://"
+  let instagramURL = URL(string: "instagram://")!
   let libraryProvider: SKAssetLibraryFacade
   weak var delegate: SKSocialMediaMediatorDelegate?
   
