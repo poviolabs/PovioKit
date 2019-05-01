645cc41eecf9a6747e1edc10cc9fd1748756c73e
Toni Kocjan <toni.kocjan@poviolabs.com>
2019-05-01 10:04:21 +0200

    PR changes

--- PovioKit/Classes/Utilities/Throttler/Throttler.swift
+++ PovioKit/Classes/Utilities/Throttler/Throttler.swift
@@ -2,32 +2,33 @@
 //  Throttler.swift
 //  PovioKit
 //
-//  Created by DK on 15/05/2018.
-//  Copyright © 2018 Povio Labs. All rights reserved.
+//  Created by Domagoj Kulundzic on 1/05/2019.
+//  Copyright © 2019 Povio Labs. All rights reserved.
 //
 
 import Foundation
 
 public class Throttler {
-	private let queue: DispatchQueue
-	private var job: DispatchWorkItem?
-	public var delay: TimeInterval
-	
-	public init(queue: DispatchQueue = .main, delay: TimeInterval = 0.5) {
-		self.queue = queue
-		self.delay = delay
-	}
+  private let queue: DispatchQueue
+  private var job: DispatchWorkItem?
+  public var delay: DispatchTimeInterval
+  
+  public init(queue: DispatchQueue = .main, delay: DispatchTimeInterval) {
+    self.queue = queue
+    self.delay = delay
+  }
 }
 
 public extension Throttler {
-	public func execute(work: @escaping () -> Void) {
-		job?.cancel()
-		let newJob = DispatchWorkItem(block: work)
-		job = newJob
-		queue.asyncAfter(deadline: .now() + delay, execute: newJob)
-	}
-	
-	public func cancelPendingJob() {
-		job?.cancel()
-	}
+  func execute(work: @escaping () -> Void) {
+    cancelPendingJob()
+    
+    let newJob = DispatchWorkItem(block: work)
+    job = newJob
+    queue.asyncAfter(deadline: .now() + delay, execute: newJob)
+  }
+  
+  func cancelPendingJob() {
+    job?.cancel()
+  }
 }
