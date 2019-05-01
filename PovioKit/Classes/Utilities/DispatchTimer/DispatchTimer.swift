d11b78cfee30df53f3e0a9706874fa49dafada36
Borut Tomazin <borut.tomazin@icloud.com>
2019-05-01 21:44:34 +0200

    Public access.

--- PovioKit/Classes/Utilities/DispatchTimer/DispatchTimer.swift
+++ PovioKit/Classes/Utilities/DispatchTimer/DispatchTimer.swift
@@ -9,8 +9,10 @@
 import Foundation
 
 /// A NSTimer replacement using GCD.
-class DispatchTimer {
+public class DispatchTimer {
   private var timer: DispatchSourceTimer?
+  
+  public init() {}
   
   deinit {
     stop()
@@ -18,9 +20,9 @@ class DispatchTimer {
 }
 
 // MARK: - Public Methods
-extension DispatchTimer {
+public extension DispatchTimer {
   /// Creates and schedules repeating timer or one time execution afer given time interval
-  func schedule(interval: DispatchTimeInterval, repeating: Bool, on queue: DispatchQueue, _ completion: (() -> Swift.Void)?) {
+  public func schedule(interval: DispatchTimeInterval, repeating: Bool, on queue: DispatchQueue, _ completion: (() -> Swift.Void)?) {
     timer = DispatchSource.makeTimerSource(flags: .strict, queue: queue)
     switch repeating {
     case true:
@@ -39,14 +41,14 @@ extension DispatchTimer {
   }
   
   /// Creates and returns `DispatchTimer` object and schedules repeating timer or one time execution after given time interval
-  static func scheduled(interval: DispatchTimeInterval, repeating: Bool, on queue: DispatchQueue, _ completion: (() -> Swift.Void)?) -> DispatchTimer {
+  public static func scheduled(interval: DispatchTimeInterval, repeating: Bool, on queue: DispatchQueue, _ completion: (() -> Swift.Void)?) -> DispatchTimer {
     let timer = DispatchTimer()
     timer.schedule(interval: interval, repeating: repeating, on: queue, completion)
     return timer
   }
   
   /// Stops dispatch scheduler
-  func stop() {
+  public func stop() {
     timer?.cancel()
     timer = nil
   }
