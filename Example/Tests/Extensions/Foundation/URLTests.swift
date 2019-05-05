d0691b5d0793b837b71fa909901f103ec0a5c6ad
Borut Tomazin <borut.tomazin@icloud.com>
2019-05-05 20:53:50 +0200

    Added some tests.

---
+++ Example/Tests/Extensions/Foundation/URLTests.swift
@@ -0,0 +1,23 @@
+//
+//  URLTests.swift
+//  PovioKit_Tests
+//
+//  Created by Borut Tomažin on 05/05/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import PovioKit
+
+class URLTests: XCTestCase {
+  func testInitWithString() {
+    let urlString = "https://github.com/poviolabs/PovioKit"
+    XCTAssertEqual(URL(string: urlString)?.absoluteString, urlString)
+    XCTAssertNil(URL(string: "")?.absoluteString)
+  }
+  
+  func testInitWithStringLiteral() {
+    let url: URL = "https://github.com/poviolabs/PovioKit"
+    XCTAssertNotNil(url)
+  }
+}
