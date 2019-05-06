f5b442b5b5cc3e594b1a4d889e179ab62730acac
Borut Tomazin <borut.tomazin@icloud.com>
2019-05-06 08:22:36 +0200

    Chore/Travis (#22)
    * Travis config.
    
    * Dev team.
    
    * Fixed scheme.
    
    * Added Travis status.
    
    * Added some tests.

--- PovioKit/Classes/Extensions/Foundation/String+Povio.swift
+++ PovioKit/Classes/Extensions/Foundation/String+Povio.swift
@@ -11,7 +11,7 @@ import Foundation
 public extension String {
   /// Returns localized string for the current locale. If the key doesn't exist, `self` is returned.
   func localized(_ args: CVarArg...) -> String {
-    guard !self.isEmpty else { return self }
+    guard !isEmpty else { return self }
     let localizedString = NSLocalizedString(self, comment: "")
     return withVaList(args) { NSString(format: localizedString, locale: Locale.current, arguments: $0) as String }
   }
