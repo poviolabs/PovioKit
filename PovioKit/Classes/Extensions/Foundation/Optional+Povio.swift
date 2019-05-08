cc4ff7543942d7883cb78356ff54cb6c71eb6329
Toni Kocjan <toni.kocjan@poviolabs.com>
2019-05-08 12:58:46 +0200

    FIxes

--- PovioKit/Classes/Extensions/Foundation/Optional+Povio.swift
+++ PovioKit/Classes/Extensions/Foundation/Optional+Povio.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-extension Optional where Wrapped: Collection {
+public extension Optional where Wrapped: Collection {
   var isNilOrEmpty: Bool {
     switch self {
     case let collection?:
