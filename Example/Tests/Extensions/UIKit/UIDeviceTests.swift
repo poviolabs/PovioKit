ea045314c5a716ace7c5a07db431b43702b93838
Klemen Zagar <klemen.zagar91@gmail.com>
2019-12-19 15:11:39 +0100

    Feature/UIDevice tests (#48)
    * Add tests for UIDevice
    
    * Remove redundant new lines. Change XCTAssert() to XCTAssertFalse()
    
    * Cleanup

---
+++ Example/Tests/Extensions/UIKit/UIDeviceTests.swift
@@ -0,0 +1,22 @@
+//
+//  UIDeviceTests.swift
+//  PovioKit_Tests
+//
+//  Created by Klemen Zagar on 05/12/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import PovioKit
+
+class UIDeviceTests: XCTestCase {
+  func testAppVariablesNotEmpty() {
+    let sut = UIDevice.current
+    XCTAssertFalse(sut.osVersion.isEmpty, "OS version should not be empty")
+    XCTAssertFalse(sut.deviceName.isEmpty, "Device name should not be empty")
+    XCTAssertFalse(sut.deviceCodeName.isEmpty, "Device code name should not be empty")
+    XCTAssertFalse(sut.appName.isEmpty, "App name should not be empty")
+    XCTAssertFalse(sut.appVersion.isEmpty, "App version should not be empty")
+    XCTAssertFalse(sut.appBuild.isEmpty, "App build number should not be empty")
+  }
+}
