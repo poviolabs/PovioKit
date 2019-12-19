294bc7d66b4083eaae29842cb6effdc9cceedcf1
Klemen Zagar <klemen.zagar91@gmail.com>
2019-12-19 14:32:15 +0100

    Remove redundant new lines. Change XCTAssert() to XCTAssertFalse()

--- Example/Tests/Extensions/UIKit/UIDeviceTests.swift
+++ Example/Tests/Extensions/UIKit/UIDeviceTests.swift
@@ -10,15 +10,13 @@ import XCTest
 @testable import PovioKit
 
 class UIDeviceTests: XCTestCase {
-  
     func testAppVariablesNotEmpty() {
       let sut = UIDevice.current
-      XCTAssert(!sut.osVersion.isEmpty, "OS version should not be empty")
-      XCTAssert(!sut.deviceName.isEmpty, "Device name should not be empty")
-      XCTAssert(!sut.deviceCodeName.isEmpty, "Device code name should not be empty")
-      XCTAssert(!sut.appName.isEmpty, "App name should not be empty")
-      XCTAssert(!sut.appVersion.isEmpty, "App version should not be empty")
-      XCTAssert(!sut.appBuild.isEmpty, "App build number should not be empty")
+      XCTAssertFalse(sut.osVersion.isEmpty, "OS version should not be empty")
+      XCTAssertFalse(sut.deviceName.isEmpty, "Device name should not be empty")
+      XCTAssertFalse(sut.deviceCodeName.isEmpty, "Device code name should not be empty")
+      XCTAssertFalse(sut.appName.isEmpty, "App name should not be empty")
+      XCTAssertFalse(sut.appVersion.isEmpty, "App version should not be empty")
+      XCTAssertFalse(sut.appBuild.isEmpty, "App build number should not be empty")
     }
-  
 }
