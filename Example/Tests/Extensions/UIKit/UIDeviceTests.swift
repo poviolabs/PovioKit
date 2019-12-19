1536350a595b9f806bb4888da79dfae04857728c
Klemen Zagar <klemen.zagar91@gmail.com>
2019-12-19 15:09:15 +0100

    Cleanup

--- Example/Tests/Extensions/UIKit/UIDeviceTests.swift
+++ Example/Tests/Extensions/UIKit/UIDeviceTests.swift
@@ -10,13 +10,13 @@ import XCTest
 @testable import PovioKit
 
 class UIDeviceTests: XCTestCase {
-    func testAppVariablesNotEmpty() {
-      let sut = UIDevice.current
-      XCTAssertFalse(sut.osVersion.isEmpty, "OS version should not be empty")
-      XCTAssertFalse(sut.deviceName.isEmpty, "Device name should not be empty")
-      XCTAssertFalse(sut.deviceCodeName.isEmpty, "Device code name should not be empty")
-      XCTAssertFalse(sut.appName.isEmpty, "App name should not be empty")
-      XCTAssertFalse(sut.appVersion.isEmpty, "App version should not be empty")
-      XCTAssertFalse(sut.appBuild.isEmpty, "App build number should not be empty")
-    }
+  func testAppVariablesNotEmpty() {
+    let sut = UIDevice.current
+    XCTAssertFalse(sut.osVersion.isEmpty, "OS version should not be empty")
+    XCTAssertFalse(sut.deviceName.isEmpty, "Device name should not be empty")
+    XCTAssertFalse(sut.deviceCodeName.isEmpty, "Device code name should not be empty")
+    XCTAssertFalse(sut.appName.isEmpty, "App name should not be empty")
+    XCTAssertFalse(sut.appVersion.isEmpty, "App version should not be empty")
+    XCTAssertFalse(sut.appBuild.isEmpty, "App build number should not be empty")
+  }
 }
