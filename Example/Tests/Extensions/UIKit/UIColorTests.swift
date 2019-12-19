487a2dbbf90c761977b2cb8b2c7b790820a4a6c2
Klemen Zagar <klemen.zagar91@gmail.com>
2019-12-19 14:58:50 +0100

    Refactor: remove print() calls, reduced number of tests

--- Example/Tests/Extensions/UIKit/UIColorTests.swift
+++ Example/Tests/Extensions/UIKit/UIColorTests.swift
@@ -10,31 +10,14 @@ import XCTest
 @testable import PovioKit
 
 class UIColorTests: XCTestCase {
-
-  func testIfBlack() {
-    let color = UIColor(red: 0, green: 0, blue: 0)
-    XCTAssert(color.isEqual(UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)), "Created color should be black")
-  }
-  
-  func testIfWhite() {
-    let color = UIColor(red: 255, green: 255, blue: 255)
-    print(color)
-    print(UIColor(red: 1, green: 1, blue: 1))
-    XCTAssert(color.isEqual(UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), "Created color should be white")
-  }
-  
   func testIfRed() {
     let color = UIColor(red: 255, green: 0, blue: 0)
     XCTAssert(color.isEqual(UIColor.red), "Created color should be red")
   }
   
-  func testIfGreen() {
-    let color = UIColor(red: 0, green: 255, blue: 0)
-    XCTAssert(color.isEqual(UIColor.green), "Created color should be green")
-  }
-  
-  func testIfBlue() {
-    let color = UIColor(red: 0, green: 0, blue: 255)
-    XCTAssert(color.isEqual(UIColor.blue), "Created color should be blue")
+  func testIfRedWithAlpha() {
+    let alpha: CGFloat = 0.5
+    let color = UIColor(red: 255, green: 0, blue: 0, alpha: alpha)
+    XCTAssert(color.isEqual(UIColor.red.withAlphaComponent(alpha)), "Created color should be red with alpha: \(alpha)")
   }
 }
