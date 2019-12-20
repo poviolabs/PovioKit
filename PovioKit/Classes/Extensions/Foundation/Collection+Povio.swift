bfc97b85dab3a056c135901daee1fde6e27894f3
Borut Tomazin <borut.tomazin@icloud.com>
2019-12-20 13:17:59 +0100

    Disabled MutableCollection extension.

--- PovioKit/Classes/Extensions/Foundation/Collection+Povio.swift
+++ PovioKit/Classes/Extensions/Foundation/Collection+Povio.swift
@@ -22,15 +22,16 @@ public extension Collection {
   }
 }
 
-public extension MutableCollection {
-  /// Allows setting or getting an element withed checked index bounds
-  subscript(safe index: Index) -> Iterator.Element? {
-    get {
-      return indices.contains(index) ? self[index] : nil
-    }
-    set {
-      guard let newValue = newValue, indices.contains(index) else { return }
-      self[index] = newValue
-    }
-  }
-}
+// NOTE: disabled due to the race condition issue
+//public extension MutableCollection {
+//  /// Allows setting or getting an element withed checked index bounds
+//  subscript(safe index: Index) -> Iterator.Element? {
+//    get {
+//      return indices.contains(index) ? self[index] : nil
+//    }
+//    set {
+//      guard let newValue = newValue, indices.contains(index) else { return }
+//      self[index] = newValue
+//    }
+//  }
+//}
