697009a3648bcc92f1f4cde6714a578f318bf3aa
Klemen Zagar <klemen.zagar91@gmail.com>
2019-12-20 11:03:15 +0100

    Feature/StartupProcessService tests (#50)
    * Add tests for StartupProcessService
    
    * Make mock class private

---
+++ Example/Tests/Utilities/StartupService/StartupProcessServiceTests.swift
@@ -0,0 +1,27 @@
+//
+//  StartupProcessServiceTests.swift
+//  PovioKit_Tests
+//
+//  Created by Klemen Zagar on 05/12/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import PovioKit
+
+class StartupProcessServiceTests: XCTestCase {
+  func testShouldCompleteProccesWhenCallExecution() {
+    let sut = StartupProcessService()
+    let mock = MockedStartupService()
+    sut.execute(process: mock)
+    XCTAssertEqual(mock.completed, true)
+  }
+}
+
+private class MockedStartupService: StartupProcess {
+  var completed: Bool = false
+  func run(completion: @escaping (Bool) -> Void) {
+    completed = true
+    return
+  }
+}
