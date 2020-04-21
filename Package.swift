30850a12e2310a20097d8f2b5f4bf3f86dc68245
Borut Tomazin <borut.tomazin@icloud.com>
2020-04-21 21:10:19 +0200

    Package update.

--- Package.swift
+++ Package.swift
@@ -1,16 +1,33 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
   name: "PovioKit",
   platforms: [
-    .macOS(.v10_14), .iOS(.v11), .tvOS(.v10), .watchOS(.v3)
+    .iOS(.v11),
+    .macOS(.v10_14),
+    .tvOS(.v10),
+    .watchOS(.v3)
   ],
   products: [
-    .library(name: "PovioKit", targets: ["PovioKit"])
+    .library(name: "PovioKit", targets: ["PovioKit"]),
+    .library(name: "PovioKitNetworking", targets: ["PovioKitNetworking"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("5.1.0"))
   ],
   targets: [
-    .target(name: "PovioKit", path: "PovioKit")
+    .target(
+      name: "PovioKit",
+      path: "Sources",
+      exclude: ["Networking"]
+    ),
+    .target(
+      name: "PovioKitNetworking",
+      dependencies: ["PovioKit", "Alamofire"],
+      path: "Sources",
+      sources: ["Networking"]
+    )
   ],
   swiftLanguageVersions: [.v5]
 )
