0d5b194f357af81f15ec1366d7e0d9517ae11ca4
Borut Tomazin <borut.tomazin@icloud.com>
2020-11-09 10:23:50 +0100

    Modified access label.

--- Sources/Views/PaddingLabel/PaddingLabel.swift
+++ Sources/Views/PaddingLabel/PaddingLabel.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 /// A UILabel subclass with configurable `contentInset`.
-class PaddingLabel: UILabel {
+open class PaddingLabel: UILabel {
   var contentInset: UIEdgeInsets = .init(top: 5, left: 5, bottom: 5, right: 5) {
     didSet { invalidateIntrinsicContentSize() }
   }
   
-  override func drawText(in rect: CGRect) {
+  override open func drawText(in rect: CGRect) {
     super.drawText(in: rect.inset(by: contentInset))
   }
   
-  override var intrinsicContentSize: CGSize {
+  override open var intrinsicContentSize: CGSize {
     var contentSize = super.intrinsicContentSize
     contentSize.height += contentInset.top + contentInset.bottom
     contentSize.width += contentInset.left + contentInset.right
