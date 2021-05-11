3f433075bd0b174019e8f47ec1182cc80fc62a12
Egzon Arifi <EgzonArifi@users.noreply.github.com>
2021-05-11 21:54:39 +0200

    Feature/Data source protocols (#107)
    * Add data source protocols
    
    * Add readme
    
    * Address PR comment

---
+++ Sources/Protocols/DataSource/RowType.swift
@@ -0,0 +1,11 @@
+//
+//  RowType.swift
+//  PovioKit
+//
+//  Created by Egzon Arifi on 09/03/2021.
+//  Copyright © 2021 Povio Labs. All rights reserved.
+//
+
+import Foundation
+
+public protocol RowType { }
