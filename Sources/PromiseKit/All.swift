b1adee24f0e7671a343d1d396f033323519aedf7
Toni Kocjan <toni.kocjan@poviolabs.com>
2021-10-18 13:15:21 +0200

    Feature/PromiseKit - Concurrent dispatch (#122)
    * Concurrent dispatch
    
    * Minor naming change
    
    * Docs update

--- Sources/PromiseKit/All.swift
+++ Sources/PromiseKit/All.swift
@@ -24,7 +24,7 @@ public func all<T, C: Collection>(
   }
   
   return .init { seal in
-    let barrier = DispatchQueue(label: "combineQueue", attributes: .concurrent)
+    let barrier = DispatchQueue(label: "com.poviokit.promisekit.barrier", attributes: .concurrent)
     for promise in promises {
       promise.finally { result in
         switch result {
