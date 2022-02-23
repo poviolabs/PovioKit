6c8deb86cec5cdc47b5873be8346fbaaf8cc67d4
Borut Tomazin <borut@povio.com>
2022-02-23 08:34:35 +0100

    Headers update.

--- Sources/Core/Extensions/UIKit/UICollectionView+Povio.swift
+++ Sources/Core/Extensions/UIKit/UICollectionView+Povio.swift
@@ -3,7 +3,7 @@
 //  PovioKit
 //
 //  Created by Povio Team on 26/4/2019.
-//  Copyright © 2021 Povio Inc. All rights reserved.
+//  Copyright © 2022 Povio Inc. All rights reserved.
 //
 
 import UIKit
