0ac9d9a4a5e7c635e9a09fb4f21ab50bc6191b5d
Arlind Dushi <dushi.arlind@gmail.com>
2022-03-10 16:06:08 +0100

    Get image data

--- Sources/Core/Views/ProfileImageView/ProfileImageView.swift
+++ Sources/Core/Views/ProfileImageView/ProfileImageView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 
 @available(iOS 13, *)
 public struct ProfileImageView: View {
-    public enum Badging {
+    public var imageTapped: (() -> Void)?
+    @State public var cornerRadius: CGFloat = 0
+    @State public var contentMode: ContentMode = .fit
+    @State public var borderColor: Color = .clear
+    @State public var borderWidth: CGFloat = 0
+    @State public var badgeAlignment: Alignment = .bottomTrailing
+    @State public var badging: BadgingMode = .none //.some(badge: .init(image: Image(systemName: "scribble"), backgroundColor: .green, borderColor: nil, borderWidth: nil))
+    @State private var urlData: Data?
+    @State public var placeHolder = UIImage(systemName: "person")
+    
+    public enum BadgingMode {
         case none
         case some(badge: BadgeStyle)
     }
@@ -22,37 +32,37 @@ public struct ProfileImageView: View {
         let borderWidth: CGFloat?
     }
     
-    var imageTapped: (() -> Void)?
-    @State public var cornerRadius: CGFloat = 0
-    @State public var contentMode: ContentMode = .fit
-    @State public var borderColor: Color = .clear
-    @State public var borderWidth: CGFloat = 0
-    @State public var badgeAlignment: Alignment = .bottomTrailing
-    @State public var badging: Badging = .some(badge: .init(image: Image(systemName: "scribble"), backgroundColor: .green, borderColor: nil, borderWidth: nil))
-    
     public init() {}
     
     public var body: some View {
         GeometryReader { geo in
             ZStack(alignment: badgeAlignment) {
-                Image(systemName: "person")
+                constructProfileImage(data: urlData, placeHolder: placeHolder!)
                     .resizable()
                     .aspectRatio(contentMode: contentMode)
-                    .frame(width: 100, height: 100)
-//                    .frame(width: geo.size.width, height: geo.size.height)
+                    .frame(width: geo.size.width, height: geo.size.height)
                     .cornerRadius(cornerRadius)
                     .border(borderColor, width: borderWidth)
                     .gesture(TapGesture().onEnded(
                         { imageTapped?() } ))
-                getBadge(badingStyle: badging, size: .init(width: 100 / 4, height: 100 / 4))
+                Badge(style: badging, size: .init(width: geo.size.width / 4, height: geo.size.height / 4))
             }
         }
     }
     
-    
     struct Badge: View {
         @State var style: BadgeStyle
         @State var size: CGSize
+        
+        init?(style: BadgingMode, size: CGSize) {
+            switch style {
+            case .none:
+                return nil
+            case .some(let badge):
+                self.style = badge
+                self.size = size
+            }
+        }
         
         var body: some View {
             style.image
@@ -67,12 +77,37 @@ public struct ProfileImageView: View {
         }
     }
     
-    private func getBadge(badingStyle: Badging, size: CGSize) -> Badge? {
-        switch badingStyle {
+    private func constructProfileImage(data: Data?, placeHolder: UIImage) -> Image {
+        switch data {
+        case .some(let data):
+            if let createdImage =  UIImage(data: data) {
+                return Image(uiImage: createdImage)
+            } else {
+                return Image(uiImage: placeHolder)
+            }
         case .none:
-            return nil
-        case .some(let style):
-            return Badge(style: style, size: size)
+            return Image(uiImage: placeHolder)
+        }
+    }
+}
+
+// MARK: - Public Methods
+@available(iOS 13, *)
+public extension ProfileImageView {
+    func set(_ url: URL?) {
+        switch url {
+        case .some(let url):
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    DispatchQueue.main.async {
+                        self.urlData = data
+                    }
+                }
+            }.resume()
+        case .none:
+            DispatchQueue.main.async {
+                self.urlData = nil
+            }
         }
     }
 }
