ac9ca2734b3dc7ca9527af2b6e3d7cc2a8549b21
Arlind Dushi <dushi.arlind@gmail.com>
2022-03-14 14:05:27 +0100

    Spacing and UIImage to Image for better scaling

--- Sources/Core/Views/ProfileImageView/ProfileImageView.swift
+++ Sources/Core/Views/ProfileImageView/ProfileImageView.swift
@@ -10,151 +10,159 @@ import SwiftUI
 
 @available(iOS 13, *)
 public class ProfileImageProperties: ObservableObject {
-    @Published public var placeHolder = UIImage(systemName: "person")
-    @Published public var backgroundType = ProfileImageView.BackgroundType.plain(.clear)
-    @Published public var cornerRadius: CGFloat = 0
-    @Published public var contentMode: ContentMode = .fit
-    @Published public var borderColor: Color = .clear
-    @Published public var borderWidth: CGFloat = 0
-    @Published public var badging: ProfileImageView.BadgingMode = .none
-    @Published public var badgeAlignment: Alignment = .bottomTrailing
-    @Published var urlData: Data?
+  @Published public var placeHolder = Image(systemName: "person")
+  @Published public var backgroundType = ProfileImageView.BackgroundType.plain(.clear)
+  @Published public var cornerRadius: CGFloat = 0
+  @Published public var contentMode: ContentMode = .fit
+  @Published public var borderColor: Color = .clear
+  @Published public var borderWidth: CGFloat = 0
+  @Published public var badging: ProfileImageView.BadgingMode = .none
+  @Published public var badgeAlignment: Alignment = .bottomTrailing
+  @Published var urlData: Data?
 }
 
 @available(iOS 13, *)
 public struct ProfileImageView: View {
-    public var imageTapped: (() -> Void)?
-    @ObservedObject public var properties = ProfileImageProperties()
+  public var imageTapped: (() -> Void)?
+  public var badgeTapped: (() -> Void)?
+  @ObservedObject public var properties = ProfileImageProperties()
+  
+  public struct BadgeStyle {
+    let image: Image
+    let backgroundColor: Color
+    let borderColor: Color?
+    let borderWidth: CGFloat?
+  }
+  
+  public enum BadgingMode {
+    case none
+    case some(badge: BadgeStyle)
+  }
+  
+  public enum BackgroundType {
+    case plain(Color)
+    case linearGradient(LinearGradient)
+    case radialGradient(RadialGradient)
+    case angularGradient(AngularGradient)
+  }
+  
+  public init() {}
+  
+  public var body: some View {
+    GeometryReader { geo in
+      ZStack(alignment: properties.badgeAlignment) {
+        ProfileImage(properties: properties, profileTapped: triggerImageTapClosure)
+        Badge(style: properties.badging, size: .init(width: geo.size.width / 3.5, height: geo.size.height / 3.5), badgeTapped: tiggerBadgeTapClosure)
+      }
+    }
+  }
+  
+  private func triggerImageTapClosure() { imageTapped?() }
+  private func tiggerBadgeTapClosure() { badgeTapped?() }
+  
+  private struct Badge: View {
+    @State var style: BadgeStyle
+    @State var size: CGSize
+    var badgeTapped: (() -> Void)
+
     
-    public struct BadgeStyle {
-        let image: Image
-        let backgroundColor: Color
-        let borderColor: Color?
-        let borderWidth: CGFloat?
+    init?(style: BadgingMode, size: CGSize, badgeTapped: @escaping () -> ()) {
+      self.badgeTapped = badgeTapped
+      
+      switch style {
+      case .none:
+        return nil
+      case .some(let badge):
+        self.style = badge
+        self.size = size
+      }
     }
     
-    public enum BadgingMode {
-        case none
-        case some(badge: BadgeStyle)
+    var body: some View {
+      style.image
+        .foregroundColor(.white)
+        .frame(width: size.width, height: size.height)
+        .background(style.backgroundColor)
+        .clipShape(Circle())
+        .gesture(TapGesture().onEnded({ badgeTapped() }))
+        .overlay(Circle().stroke(
+          (style.borderColor != nil) ? style.borderColor! : Color.white,
+          lineWidth: (style.borderWidth != nil) ? style.borderWidth! : 2)
+        )
+    }
+  }
+  
+  private struct ProfileImage: View {
+    @ObservedObject var properties: ProfileImageProperties
+    var profileTapped: (() -> Void)
+    var optionalPlaceHolder: UIImage?
+    
+    var body: some View {
+      GeometryReader { geo in
+        constructProfileImage(data: properties.urlData, placeHolder: properties.placeHolder)
+          .resizable()
+          .aspectRatio(contentMode: properties.contentMode)
+          .frame(width: geo.size.width, height: geo.size.height)
+          .gesture(TapGesture().onEnded({ profileTapped() }))
+          .background(getBackground())
+          .cornerRadius(properties.cornerRadius)
+          .border(properties.borderColor, width: properties.borderWidth)
+      }
     }
     
-    public enum BackgroundType {
-        case plain(Color)
-        case linearGradient(LinearGradient)
-        case radialGradient(RadialGradient)
-        case angularGradient(AngularGradient)
+    private func constructProfileImage(data: Data?, placeHolder: Image) -> Image {
+      switch data {
+      case .some(let data):
+        if let createdImage =  UIImage(data: data) {
+          return Image(uiImage: createdImage)
+        } else {
+          return placeHolder
+        }
+      case .none:
+        return placeHolder
+      }
     }
     
-    public init() {}
-    
-    public var body: some View {
-        GeometryReader { geo in
-            ZStack(alignment: properties.badgeAlignment) {
-                ProfileImage(properties: properties, profileTapped: triggerImageTapClosure)
-                Badge(style: properties.badging, size: .init(width: geo.size.width / 4, height: geo.size.height / 4))
-            }
-        }
+    private func getBackground() -> AnyView {
+      switch properties.backgroundType {
+      case .plain(let backgroundColor):
+        return AnyView(backgroundColor)
+      case let .linearGradient(gradient):
+        return AnyView(gradient)
+      case let .angularGradient(gradient):
+        return AnyView(gradient)
+      case let .radialGradient(gradient):
+        return AnyView(gradient)
+      }
     }
-    
-    private func triggerImageTapClosure() { imageTapped?() }
-    
-    private struct Badge: View {
-        @State var style: BadgeStyle
-        @State var size: CGSize
-        
-        init?(style: BadgingMode, size: CGSize) {
-            switch style {
-            case .none:
-                return nil
-            case .some(let badge):
-                self.style = badge
-                self.size = size
-            }
-        }
-        
-        var body: some View {
-            style.image
-                .foregroundColor(.white)
-                .frame(width: size.width, height: size.height)
-                .background(style.backgroundColor)
-                .clipShape(Circle())
-                .overlay(Circle().stroke(
-                    (style.borderColor != nil) ? style.borderColor! : Color.white,
-                    lineWidth: (style.borderWidth != nil) ? style.borderWidth! : 2)
-                )
-        }
-    }
-    
-    private struct ProfileImage: View {
-        @ObservedObject var properties: ProfileImageProperties
-        var profileTapped: (() -> Void)
-        
-        var body: some View {
-            GeometryReader { geo in
-                constructProfileImage(data: properties.urlData, placeHolder: properties.placeHolder!)
-                    .resizable()
-                    .aspectRatio(contentMode: properties.contentMode)
-                    .frame(width: geo.size.width, height: geo.size.height)
-                    .background(getBackground())
-                    .cornerRadius(properties.cornerRadius)
-                    .border(properties.borderColor, width: properties.borderWidth)
-                    .gesture(TapGesture().onEnded( { profileTapped() } ))
-            }
-        }
-        
-        private func constructProfileImage(data: Data?, placeHolder: UIImage) -> Image {
-            switch data {
-            case .some(let data):
-                if let createdImage =  UIImage(data: data) {
-                    return Image(uiImage: createdImage)
-                } else {
-                    return Image(uiImage: placeHolder)
-                }
-            case .none:
-                return Image(uiImage: placeHolder)
-            }
-        }
-        
-        private func getBackground() -> AnyView {
-            switch properties.backgroundType {
-            case .plain(let backgroundColor):
-                return AnyView(backgroundColor)
-            case let .linearGradient(gradient):
-                return AnyView(gradient)
-            case let .angularGradient(gradient):
-                return AnyView(gradient)
-            case let .radialGradient(gradient):
-                return AnyView(gradient)
-            }
-        }
-    }
+  }
 }
 
 // MARK: - Public Methods
 @available(iOS 13, *)
 public extension ProfileImageView {
-    func set(_ url: URL?) {
-        switch url {
-        case .some(let url):
-            URLSession.shared.dataTask(with: url) { data, response, error in
-                if let data = data {
-                    DispatchQueue.main.async {
-                        properties.urlData = data
-                    }
-                }
-            }.resume()
-        case .none:
-            DispatchQueue.main.async {
-                properties.urlData = nil
-            }
+  func set(_ url: URL?) {
+    switch url {
+    case .some(let url):
+      URLSession.shared.dataTask(with: url) { data, response, error in
+        if let data = data {
+          DispatchQueue.main.async {
+            properties.urlData = data
+          }
         }
+      }.resume()
+    case .none:
+      DispatchQueue.main.async {
+        properties.urlData = nil
+      }
     }
+  }
 }
 
 @available(iOS 13, *)
 struct ProfileImageView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileImageView()
-    }
+  static var previews: some View {
+    ProfileImageView()
+  }
 }
 
