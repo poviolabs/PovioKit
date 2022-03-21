2abceb5a6c8fad2b614e529b2771a6baf192f8dd
Yll Fejziu <38266916+yllfejziu@users.noreply.github.com>
2022-03-21 09:03:26 +0100

    Register cells using variadic arguments (#143)
    * Register cells using variadic arguments
    
    * Address PR comments

--- Sources/Core/Extensions/UIKit/UITableView+Povio.swift
+++ Sources/Core/Extensions/UIKit/UITableView+Povio.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 public extension UITableView {
+  @available(*, deprecated, renamed: "register(_:)")
   func register<T: UITableViewCell>(cell: T.Type) {
     register(T.self, forCellReuseIdentifier: T.identifier)
   }
   
+  @available(*, deprecated, renamed: "register(_:)")
   func register<T: UITableViewHeaderFooterView>(headerFooterView: T.Type) {
     register(T.self, forHeaderFooterViewReuseIdentifier: T.identifier)
+  }
+  
+  func register(_ cells: UITableViewCell.Type...) {
+    cells.forEach { register($0.self, forCellReuseIdentifier: $0.identifier) }
+  }
+  
+  func register(_ headerFooterViews: UITableViewHeaderFooterView.Type...) {
+    headerFooterViews.forEach { register($0.self, forHeaderFooterViewReuseIdentifier: $0.identifier) }
   }
   
   func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
