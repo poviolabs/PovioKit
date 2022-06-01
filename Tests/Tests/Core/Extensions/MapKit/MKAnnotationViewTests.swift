16504a49f9f458821218de6d958c24805d732dbf
Gentian Barileva <gentianbarileva@gmail.com>
2022-06-02 00:36:53 +0200

    Guarantee `identifier` returns correct identifier.

---
+++ Tests/Tests/Core/Extensions/MapKit/MKAnnotationViewTests.swift
@@ -0,0 +1,25 @@
+//
+//  MKAnnotationViewTests.swift
+//  PovioKit_Tests
+//
+//  Created by Gentian Barileva on 02/06/2022.
+//  Copyright © 2022 Povio Inc. All rights reserved.
+//
+
+import XCTest
+import PovioKit
+import MapKit.MKAnnotationView
+
+class MKAnnotationViewTests: XCTestCase {
+  func test_identifier_returnsCorrectIdentifier() {
+    let SUTs: [(expectedIdentifier: String, mkAnnotationView: MKAnnotationView.Type)] = [("CustomMKAnnotationView", CustomMKAnnotationView.self), ("OtherMKAnnotationView", OtherMKAnnotationView.self)]
+    
+    for sut in SUTs {
+      XCTAssertEqual(sut.expectedIdentifier, sut.mkAnnotationView.identifier)
+    }
+  }
+}
+
+// MARK: - Helpers
+private class CustomMKAnnotationView: MKAnnotationView { }
+private class OtherMKAnnotationView: MKAnnotationView { }
