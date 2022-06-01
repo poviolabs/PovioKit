8444cdb06a7ed1d7c722cfde4b25cd95836c464e
Gentian Barileva <gentianbarileva@gmail.com>
2022-06-02 00:02:24 +0200

    Guarantee `firstResponder` returns correct instance.

--- Tests/Tests/Core/Extensions/UIKit/UIResponderTests.swift
+++ Tests/Tests/Core/Extensions/UIKit/UIResponderTests.swift
@@ -7,7 +7,30 @@
 //
 
 import XCTest
+import PovioKit
 
 class UIResponderTests: XCTestCase {
-  
+  func test_firstResponder_returnsCorrectType() {
+    let sut = MainViewController()
+    sut.loadView()
+    
+    let firstResponder = sut.buttonsContainerView.firstResponder(ofType: type(of: sut))
+    
+    XCTAssertNotNil(firstResponder)
+    XCTAssertEqual(sut, firstResponder)
+  }
 }
+
+// MARK: - Helpers
+private class MainViewController: UIViewController {
+  let contentView = ContentView()
+  let buttonsContainerView = ButtonsContainerView()
+  
+  override func loadView() {
+    view = contentView
+    view.addSubview(buttonsContainerView)
+  }
+}
+
+private class ContentView: UIView { }
+private class ButtonsContainerView: UIView { }
