f91f5a9d29174ba6e030288387c366953d1dab5c
Gentian Barileva <gentianbarileva@gmail.com>
2022-06-01 22:50:50 +0200

    Introduce `BundleReaderTests`.

---
+++ Tests/Tests/Core/Utilities/BundleReader/BundleReaderTests.swift
@@ -0,0 +1,13 @@
+//
+//  BundleReaderTests.swift
+//  PovioKit_Tests
+//
+//  Created by Gentian Barileva on 01/06/2022.
+//  Copyright © 2022 Povio Inc. All rights reserved.
+//
+
+import XCTest
+
+class BundleReaderTests: XCTestCase {
+  
+}
