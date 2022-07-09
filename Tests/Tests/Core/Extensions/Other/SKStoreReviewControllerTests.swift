deca8416e4605d6250daa892ddf7e94d9cf41280
Gentian Barileva <gentianbarileva@gmail.com>
2022-07-09 11:12:18 +0200

    Introduce `SKStoreReviewControllerTests`.

---
+++ Tests/Tests/Core/Extensions/Other/SKStoreReviewControllerTests.swift
@@ -0,0 +1,13 @@
+//
+//  SKStoreReviewControllerTests.swift
+//  PovioKit_Tests
+//
+//  Created by Gentian Barileva on 09/07/2022.
+//  Copyright © 2022 Povio Inc. All rights reserved.
+//
+
+import XCTest
+
+class SKStoreReviewControllerTests: XCTestCase {
+  
+}
