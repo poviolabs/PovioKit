e23cf122460e7d2529c385706179ca1a8b8826dc
Gentian Barileva <gentianbarileva@gmail.com>
2022-07-16 20:36:52 +0200

    Move code from tests to production.

--- Sources/Core/Extensions/Other/SKStoreReviewController+PovioKit.swift
+++ Sources/Core/Extensions/Other/SKStoreReviewController+PovioKit.swift
@@ -8,16 +8,69 @@
 
 import StoreKit
 
+@available(iOS 14.0, *)
 public extension SKStoreReviewController {
-  @available(iOS 14.0, *)
   /// Request a review popup on the current scene.
   ///
   /// Example: `SKStoreReviewController.requestReviewInCurrentScene()`
-  static func requestReviewInCurrentScene() {
-    (UIApplication
-      .shared
-      .connectedScenes
-      .first { $0.activationState == .foregroundActive } as? UIWindowScene
-    ).map { requestReview(in: $0) }
+  static func requestReviewInCurrentScene(sceneProvider: SceneProviding = UIApplicationSceneProvider(),
+                                          reviewProvider: RequestReviewProviding = SKStoreReviewProvider()) {
+    let sceneManager = SceneManager(provider: sceneProvider)
+    let activeScene = sceneManager.getActiveScene()
+    reviewProvider.requestReview(in: activeScene)
   }
+  
+  private struct SceneManager {
+    private let provider: SceneProviding
+    
+    public init(provider: SceneProviding) {
+      self.provider = provider
+    }
+    
+    func getActiveScene() -> Scene? {
+      provider.getConnectedScenes()
+        .first(where: { $0.activationState == .foregroundActive })
+    }
+  }
+}
+
+// MARK: Helpers
+@available(iOS 14.0, *)
+public extension SKStoreReviewController {
+  struct SKStoreReviewProvider: RequestReviewProviding {
+    public init() { }
+    
+    public func requestReview(in scene: Scene?) {
+      if let scene = scene?.ui as? UIWindowScene {
+        SKStoreReviewController.requestReview(in: scene)
+      }
+    }
+  }
+  
+  struct UIApplicationSceneProvider: SceneProviding {
+    private let connectedScenes = UIApplication.shared.connectedScenes
+    
+    public init() { }
+    
+    public func getConnectedScenes() -> [Scene] {
+      connectedScenes
+        .map { Scene(ui: $0, activationState: $0.activationState) }
+    }
+  }
+}
+
+@available(iOS 14.0, *)
+public protocol RequestReviewProviding {
+  func requestReview(in scene: Scene?)
+}
+
+@available(iOS 14.0, *)
+public protocol SceneProviding {
+  func getConnectedScenes() -> [Scene]
+}
+
+@available(iOS 14.0, *)
+public struct Scene {
+    let ui: UIScene?
+    let activationState: UIWindowScene.ActivationState
 }
--- Tests/Tests/Core/Extensions/Other/SKStoreReviewControllerTests.swift
+++ Tests/Tests/Core/Extensions/Other/SKStoreReviewControllerTests.swift
@@ -7,57 +7,15 @@
 //
 
 import XCTest
+@testable import PovioKit
 import StoreKit
-
-@available(iOS 14.0, *)
-public protocol RequestReviewProviding {
-  func requestReview(in scene: Scene?)
-}
-
-@available(iOS 14.0, *)
-public protocol SceneProviding {
-  func getConnectedScenes() -> [Scene]
-}
-
-@available(iOS 14.0, *)
-public struct UIApplicationSceneProvider: SceneProviding {
-  private let connectedScenes = UIApplication.shared.connectedScenes
-  
-  public func getConnectedScenes() -> [Scene] {
-    connectedScenes
-      .map { Scene(ui: $0, activationState: $0.activationState) }
-  }
-}
-
-@available(iOS 14.0, *)
-public struct SKStoreReviewProvider: RequestReviewProviding {
-  public func requestReview(in scene: Scene?) {
-    if let scene = scene?.ui as? UIWindowScene {
-      SKStoreReviewController.requestReview(in: scene)
-    }
-  }
-}
-
-@available(iOS 14.0, *)
-func tempRequestReviewInCurrentScene(sceneProvider: SceneProviding = UIApplicationSceneProvider(),
-                                     reviewProvider: RequestReviewProviding = SKStoreReviewProvider()) {
-  let sceneManager = SceneManager(provider: sceneProvider)
-  let activeScene = sceneManager.getActiveScene()
-  reviewProvider.requestReview(in: activeScene)
-}
-
-@available(iOS 14.0, *)
-public struct Scene {
-    let ui: UIScene?
-    let activationState: UIWindowScene.ActivationState
-}
 
 @available(iOS 14.0, *)
 class SKStoreReviewControllerTests: XCTestCase {
   func test_requestReviewInCurrentScene_messagesSceneProvider() {
     let (sceneProvider, reviewProvider) = makeSUT()
 
-    tempRequestReviewInCurrentScene(sceneProvider: sceneProvider, reviewProvider: reviewProvider)
+    SKStoreReviewController.requestReviewInCurrentScene(sceneProvider: sceneProvider, reviewProvider: reviewProvider)
     
     XCTAssertTrue(sceneProvider.didCallGetGonnectedScene)
   }
@@ -66,7 +24,8 @@ class SKStoreReviewControllerTests: XCTestCase {
     let scene = anyScene(activationState: .foregroundActive)
     let (sceneProvider, reviewProvider) = makeSUT(scenes: [scene])
     
-    tempRequestReviewInCurrentScene(sceneProvider: sceneProvider, reviewProvider: reviewProvider)
+    SKStoreReviewController.requestReviewInCurrentScene(sceneProvider: sceneProvider, reviewProvider: reviewProvider)
+    
     
     XCTAssertTrue(reviewProvider.capturedScene?.activationState == scene.activationState)
   }
@@ -75,7 +34,7 @@ class SKStoreReviewControllerTests: XCTestCase {
     let scenes = [anyScene(activationState: .unattached), anyScene(activationState: .background), anyScene(activationState: .foregroundInactive), anyScene(activationState: .unattached)]
     let (sceneProvider, reviewProvider) = makeSUT(scenes: scenes)
     
-    tempRequestReviewInCurrentScene(sceneProvider: sceneProvider, reviewProvider: reviewProvider)
+    SKStoreReviewController.requestReviewInCurrentScene(sceneProvider: sceneProvider, reviewProvider: reviewProvider)
     
     XCTAssertNil(reviewProvider.capturedScene)
   }
@@ -83,7 +42,7 @@ class SKStoreReviewControllerTests: XCTestCase {
   func test_requestReviewInCurrentScene_sceneProviderDoesntMessageReviewProviderOnEmptyScenes() {
     let (sceneProvider, reviewProvider) = makeSUT()
 
-    tempRequestReviewInCurrentScene(sceneProvider: sceneProvider, reviewProvider: reviewProvider)
+    SKStoreReviewController.requestReviewInCurrentScene(sceneProvider: sceneProvider, reviewProvider: reviewProvider)
     
     XCTAssertTrue(sceneProvider.scenes.isEmpty)
     XCTAssertNil(reviewProvider.capturedScene)
@@ -125,19 +84,5 @@ private class MockReviewProvider: RequestReviewProviding {
   
   func requestReview(in scene: Scene?) {
     capturedScene = scene
-  }
-}
-
-@available(iOS 14.0, *)
-struct SceneManager {
-  private let provider: SceneProviding
-  
-  public init(provider: SceneProviding) {
-    self.provider = provider
-  }
-  
-  func getActiveScene() -> Scene? {
-    provider.getConnectedScenes()
-      .first(where: { $0.activationState == .foregroundActive })
   }
 }
