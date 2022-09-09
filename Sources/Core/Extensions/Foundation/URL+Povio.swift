4c8e3c3ca07af6bc19a39ba40f3c740cb0b7a8d1
Borut Tomazin <borut@povio.com>
2022-09-09 08:32:50 +0200

    Code.

--- Sources/Core/Extensions/Foundation/URL+Povio.swift
+++ Sources/Core/Extensions/Foundation/URL+Povio.swift
@@ -16,12 +16,12 @@ public extension URL {
   
   /// Append parameter to the URL
   /// ```
-  /// let someURL: URL = "https://poviolabs.com"
+  /// let someURL: URL = "https://povio.com"
   /// let newURL = someURL
   ///   .appending("accept", value: "developers")
   ///   .appending("tech", value: "iOS"
   ///
-  /// print(newURL) // https://poviolabs.com?accept=developers&tech=iOS
+  /// print(newURL) // https://povio.com?accept=developers&tech=iOS
   /// ```
   func appending(_ name: String, value: String?) -> URL {
     guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else { return absoluteURL }
