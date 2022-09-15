f3a4512f1ad03536af9390584ae4e402bfaf3ab6
Gentian Barileva <gentianbarileva@gmail.com>
2022-09-15 22:25:01 +0200

    Guarantee `XCTAssertEqualURLRequest` fails on different httpBody.

--- Tests/Tests/Core/Extensions/Other/XCTestCaseTests.swift
+++ Tests/Tests/Core/Extensions/Other/XCTestCaseTests.swift
@@ -8,4 +8,24 @@
 
 import XCTest
 
-final class XCTestCaseTests: XCTestCase { }
+final class XCTestCaseTests: XCTestCase {
+  func test_XCTAssertEqualURLRequest_httpBodyFailsOnDifferentDataSets() {
+    let url = URL(string: "https://www.povio.com")!
+    let data1 = "TestData".data(using: .utf8)
+    let data2 = "Povio🚀".data(using: .utf8)
+    
+    var firstUrlRequest = URLRequest(url: url)
+    firstUrlRequest.httpBody = data1
+    
+    var secondUrlRequest = URLRequest(url: url)
+    secondUrlRequest.httpBody = data2
+    
+    // Test passes even though httpBody is different.
+    XCTAssertEqual(firstUrlRequest, secondUrlRequest)
+    
+    XCTExpectFailure("Expected test to fail since httpBody is different.") {
+      XCTAssertEqual(firstUrlRequest.httpBody, secondUrlRequest.httpBody)
+      XCTAssertEqualURLRequest(firstUrlRequest, secondUrlRequest)
+    }
+  }
+}
