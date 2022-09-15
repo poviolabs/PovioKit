fb39bb3bda5c25567b64d88ec8e143fa7f0e22c9
Gentian Barileva <gentianbarileva@gmail.com>
2022-09-15 22:24:11 +0200

    Introduce `XCTestCaseTests`

---
+++ Tests/Tests/Core/Extensions/Other/XCTestCaseTests.swift
@@ -0,0 +1,11 @@
+//
+//  XCTestCaseTests.swift
+//  PovioKit_Tests
+//
+//  Created by Borut Tomazin on 15/09/2022.
+//  Copyright © 2022 Povio Inc. All rights reserved.
+//
+
+import XCTest
+
+final class XCTestCaseTests: XCTestCase { }
