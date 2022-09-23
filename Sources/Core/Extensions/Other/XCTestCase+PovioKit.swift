c97485d1cf9af732375b552ecaa9ca970fc9046a
Gentian Barileva <gentianbarileva@gmail.com>
2022-09-23 11:04:02 +0200

    Feature/XCTAssert+URLRequest (#198)
    * Add `XCTAssertEqualURLRequest` method.
    
    * Make messages more descriptive.
    
    * Introduce `XCTestCaseTests`
    
    * Guarantee `XCTAssertEqualURLRequest` fails on different httpBody.
    
    * Guarantee `XCTAssertEqualURLRequest` has expected failures.
    
    * Guarantee `XCTAssertEqualURLRequest` has expected failures for different httpMethod.
    
    * Refactor tests.
    
    * Guarantee `XCTAssertEqualURLRequest` has expected failures for different httpHeaderFields.
    
    * Refactor tests.
    
    * Add `XCTAssertNotEqualURLRequest`.
    
    * Guarantee `XCTAssertNotEqualURLRequest` passes On Different URLRequest.
    
    * Guarantee `XCTAssertNotEqualURLRequest` passes on differentURLRequest combinations.
    
    * Add documentation.

---
+++ Sources/Core/Extensions/Other/XCTestCase+PovioKit.swift
@@ -0,0 +1,49 @@
+//
+//  XCTestCase+PovioKit.swift
+//  PovioKit
+//
+//  Created by Gentian Barileva on 09/09/2022.
+//  Copyright © 2022 Povio Inc. All rights reserved.
+//
+
+import XCTest
+
+public extension XCTestCase {
+  /**
+   Use this method when you need to thoroughly assert URLRequest.
+   
+   Asserted properties:
+   - `Self`
+   - `url`
+   - `httpMethod`
+   - `httpBody`
+   - `allHTTPHeaderFields`
+   */
+  func XCTAssertEqualURLRequest(_ lhs: URLRequest?, _ rhs: URLRequest?, file: StaticString = #filePath, line: UInt = #line) {
+    XCTAssertEqual(lhs, rhs, "Expected URLRequest to be equal", file: file, line: line)
+    guard let lhs = lhs, let rhs = rhs else { return }
+    XCTAssertEqual(lhs.url, rhs.url, "Expected url to be equal", file: file, line: line)
+    XCTAssertEqual(lhs.httpMethod, rhs.httpMethod, "Expected httpMethod to be equal", file: file, line: line)
+    XCTAssertEqual(lhs.httpBody ?? Data(), rhs.httpBody ?? Data(), "Expected httpBody to be equal", file: file, line: line)
+    XCTAssertEqual(lhs.allHTTPHeaderFields ?? [:], rhs.allHTTPHeaderFields ?? [:], "Expected HTTPHeaderFields to be equal", file: file, line: line)
+  }
+  
+  /**
+   Use this method when you need to thoroughly assert URLRequest.
+   
+   Asserted properties:
+   - `Self`
+   - `url`
+   - `httpMethod`
+   - `httpBody`
+   - `allHTTPHeaderFields`
+   */
+  func XCTAssertNotEqualURLRequest(_ lhs: URLRequest?, _ rhs: URLRequest?, file: StaticString = #filePath, line: UInt = #line) {
+    XCTAssertNotEqual(lhs, rhs, "Expected URLRequest not to be equal", file: file, line: line)
+    guard let lhs = lhs, let rhs = rhs else { return }
+    XCTAssertNotEqual(lhs.url, rhs.url, "Expected url not to be equal", file: file, line: line)
+    XCTAssertNotEqual(lhs.httpMethod, rhs.httpMethod, "Expected httpMethod not to be equal", file: file, line: line)
+    XCTAssertNotEqual(lhs.httpBody ?? Data(), rhs.httpBody ?? Data(), "Expected httpBody not to be equal", file: file, line: line)
+    XCTAssertNotEqual(lhs.allHTTPHeaderFields ?? [:], rhs.allHTTPHeaderFields ?? [:], "Expected HTTPHeaderFields not to be equal", file: file, line: line)
+  }
+}
