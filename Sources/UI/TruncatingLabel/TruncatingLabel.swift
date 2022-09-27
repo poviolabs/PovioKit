238fdc69992a1ac34c8092ca13d29158539f00f3
Toni K. Turk <kocjanturk@gmail.com>
2022-09-27 10:03:17 +0200

    Initializers before methods

--- Sources/UI/TruncatingLabel/TruncatingLabel.swift
+++ Sources/UI/TruncatingLabel/TruncatingLabel.swift
@@ -103,6 +103,15 @@ public class TruncatingLabel: UIView {
   public var secondaryText: String = "" {
     didSet { setNeedsDisplay() }
   }
+ 
+  public init() {
+    super.init(frame: .zero)
+    backgroundColor = .white
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
   
   private lazy var internalIntrinsicContentSize: CGSize = calculateIntrinsicContentSize()
   
@@ -268,15 +277,6 @@ public class TruncatingLabel: UIView {
   
   public override var intrinsicContentSize: CGSize {
     internalIntrinsicContentSize
-  }
-  
-  public init() {
-    super.init(frame: .zero)
-    backgroundColor = .white
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
   }
 }
 
