74dbeaf9d9951fb9c8d381a3c026702c3c4e6b3a
Ndriqim Nagavci <ndriqim.nagavci@povio.com>
2022-10-27 15:09:27 +0200

    Guarantee identifier returns correct UICollectionReusableView identifier.

---
+++ Tests/Tests/Core/Extensions/UIKit/UICollectionReusableViewTests.swift
@@ -0,0 +1,23 @@
+//
+//  UICollectionReusableViewTests.swift
+//  PovioKit_Tests
+//
+//  Created by Ndriqim Nagavci on 25.10.22.
+//  Copyright © 2022 Povio Inc. All rights reserved.
+//
+
+import XCTest
+import PovioKit
+
+class UICollectionReusableViewTests: XCTestCase {
+  func test_identifier_returnsCorrectIdentifier() {
+    let SUTs: [(expectedIdentifier: String, cell: UICollectionReusableView.Type)] = [("IdentifierTest", IdentifierTest.self), ("SomeCustomCell", SomeCustomCell.self)]
+    
+    for sut in SUTs {
+      XCTAssertEqual(sut.expectedIdentifier, sut.cell.identifier)
+    }
+  }
+}
+
+private class IdentifierTest: UICollectionReusableView { }
+private class SomeCustomCell: UICollectionReusableView { }
