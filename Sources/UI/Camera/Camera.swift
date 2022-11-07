3ae96a5f767f89a56a849dc0ef8cd9f0382056a1
Ndriqim Nagavci <114908711+nagavcindriqim@users.noreply.github.com>
2022-11-07 15:00:17 +0100

    Feature: [UI] Camera (#210)
    * Initial implementation
    
    * Added session queue & viewPreviewLayerOrientation for photoOutput
    
    * Possibility to switch between front facing camera and back
    
    * Add README
    
    * PR Changes:
    
    - Public CameraService
    - Async RequestCameraAuthorization
    - Updated README
    
    * PR Changes:
    
    - CameraService value type
    - Mappers for CameraPosition & MediaType
    
    * Extracted mapping of CameraPosition & MediaType from CameraService
    
    * Moved Camera enums into a standalone extension file
    
    * PR comments
    
    - Make PhotoCamera prepare method to async
    - Set cameraPosition throws
    - Replace Logger.debug with Logger.error
    - ToggleTorch throw error
    
    * Removed configureComponents from Camera, added AVLayerVideoGravity as default value
    
    * PR comments:
    
    - Move isCameraAvailable to Camera
    - Remove CameraPermissionProviding
    - PhotoCameraDelegate added reference to PhotoCamera
    
    * PhotoCamera takePhoto changes
    
    - Customization controlled by user with default values
    - Add an additional method that accepts AVCapturePhotoSettings
    
    * Add Camera deviceType
    
    * QRCodeScanner configureComponents now throws
    
    * Updated README
    
    * PR comments
    
    - PreviewLayer avoid using abbreviations
    - Code enhancments
    
    * Renamed configureComponents to configure

---
+++ Sources/UI/Camera/Camera.swift
@@ -0,0 +1,98 @@
+//
+//  Camera.swift
+//  PovioKit
+//
+//  Created by Ndriqim Nagavci on 13/10/2022.
+//  Copyright © 2022 Povio Inc. All rights reserved.
+//
+
+import AVFoundation
+import PovioKit
+
+public class Camera: NSObject {
+  var device: AVCaptureDevice? {
+    switch cameraPosition {
+    case .back:
+      return isCameraAvailable(position: .back) ? AVCaptureDevice.default(for: .video) : nil
+    case .front:
+      return isCameraAvailable(position: .front) ? AVCaptureDevice.default(deviceType, for: .video, position: .front) : nil
+    }
+  }
+  let session = AVCaptureSession()
+  // Communicate with the session and other session objects on this queue.
+  let sessionQueue = DispatchQueue(label: "com.poviokit.camera")
+  public lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+    let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+    previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+    
+    return previewLayer
+  }()
+  public let cameraService: CameraService
+  public var deviceType: AVCaptureDevice.DeviceType = .builtInWideAngleCamera
+  var cameraPosition: CameraPosition = .back
+  
+  init(with cameraService: CameraService = CameraService()) {
+    self.cameraService = cameraService
+    super.init()
+  }
+  
+  deinit {
+    stopSession()
+  }
+}
+
+// MARK: - Public Methods
+public extension Camera {
+  var isTorchAvailable: Bool {
+    device.map { $0.hasTorch && $0.isTorchAvailable } ?? false
+  }
+  
+  func requestAuthorizationStatus() async -> Bool {
+    await cameraService.requestCameraAuthorization()
+  }
+  
+  func startSession() {
+    sessionQueue.async {
+      guard !self.session.isRunning else { return }
+      self.session.startRunning()
+    }
+  }
+  
+  func stopSession() {
+    sessionQueue.async {
+      guard self.session.isRunning else { return }
+      self.session.stopRunning()
+    }
+    try? setTorch(on: false) // just in case but flashlight is automatically turned off when session is stopped
+  }
+  
+  func toggleTorch() throws {
+    try setTorch(on: !(device?.isTorchActive ?? true))
+  }
+}
+
+// MARK: - Private Methods
+private extension Camera {
+  /// Check if camera is available on device
+  func isCameraAvailable(position: Camera.CameraPosition) -> Bool {
+    !AVCaptureDevice
+      .DiscoverySession(
+        deviceTypes: [deviceType],
+        mediaType: .video,
+        position: position.asAVCaptureDevicePosition)
+      .devices
+      .isEmpty
+  }
+  
+  func setTorch(on: Bool) throws {
+    guard let device = device, device.hasTorch, device.isTorchAvailable else { return }
+    try device.lockForConfiguration()
+    switch on {
+    case true:
+      try device.setTorchModeOn(level: AVCaptureDevice.maxAvailableTorchLevel)
+    case false:
+      device.torchMode = .off
+    }
+    device.unlockForConfiguration()
+  }
+}
