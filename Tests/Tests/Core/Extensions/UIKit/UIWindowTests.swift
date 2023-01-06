32fd11320afaac8774a21818d1ce23b4a9eca0c4
Borut Tomazin <borut@povio.com>
2023-01-06 10:05:29 +0100

    Test.

---
+++ Tests/Tests/Core/Extensions/UIKit/UIWindowTests.swift
@@ -0,0 +1,23 @@
+//
+//  UIWindowTests.swift
+//  PovioKit_Tests
+//
+//  Created by Borut Tomazin on 6/1/2022.
+//  Copyright © 2023 Povio Inc. All rights reserved.
+//
+
+import XCTest
+import UIKit
+@testable import PovioKit
+
+class UIWindowTests: XCTestCase {
+  func test_safeAreaInsets() {
+    let window = UIWindow()
+    window.makeKeyAndVisible()
+    // this values work for iPhone 14 flavors
+    XCTAssertEqual(window.safeAreaInsets.top, 59)
+    XCTAssertEqual(window.safeAreaInsets.left, 0)
+    XCTAssertEqual(window.safeAreaInsets.right, 0)
+    XCTAssertEqual(window.safeAreaInsets.bottom, 34)
+  }
+}
