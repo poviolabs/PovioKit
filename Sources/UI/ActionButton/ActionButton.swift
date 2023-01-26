73487f350304891a578e493d9872418d1d8e666a
Arlind Dushi <dushi.arlind@gmail.com>
2023-01-26 15:04:33 +0100

    Refactor

--- Sources/UI/ActionButton/ActionButton.swift
+++ Sources/UI/ActionButton/ActionButton.swift
@@ -9,21 +9,27 @@
 import SwiftUI
 
 private class ActionButtonViewModel: ObservableObject {
-  @Published public var title: String = ""
-  @Published public var font: Font = .system(size: 18)
+  @Published public var title: String = "Button"
+  @Published public var font: Font = .system(size: 16)
   @Published public var textColor: Color = .white
-  @Published public var cornerRadius: ActionButton.CornerRadiusType = .custom(0)
+  @Published public var cornerRadius: ActionButton.CornerRadiusType = .rounded
   @Published public var backgroundType = ActionButton.Background.plain(.blue)
   @Published public var borderColor: Color = .clear
   @Published public var borderWidth: CGFloat = 0
-  @Published public var extraImage: ActionButton.ExtraImage?
+  @Published public var leftImage: ActionButton.ExtraImage?
+  @Published public var rightImage: ActionButton.ExtraImage?
+  @Published public var titleLeftImage: ActionButton.ExtraImage?
+  @Published public var titleRightImage: ActionButton.ExtraImage?
+  @Published public var actionHandler: (() -> Void)?
 }
 
-public struct ActionButton: View {
+struct ActionButton: View {
   @ObservedObject private var properties = ActionButtonViewModel()
-  private var actionHandler: (() -> Void)?
   
   public init() {}
+  public init(action: @escaping (() -> Void)) {
+    properties.actionHandler = action
+  }
   
   public enum Background {
     case plain(Color)
@@ -33,21 +39,66 @@ public struct ActionButton: View {
   }
   
   public var body: some View {
-    GeometryReader { geo in
-      ZStack(alignment: getAlignment(for: properties.extraImage)) {
-        ButtonView(size: geo.size, action: buttonAction, properties: properties)
-        ExtraImageView(imageAlignment: properties.extraImage, tintColor: properties.textColor, size: geo.size)
+    Button(action: {
+      properties.actionHandler?()
+    }) {
+      HStack {
+        properties.leftImage?.image
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width: properties.leftImage?.size.width,
+                 height: properties.leftImage?.size.height)
+        Spacer()
+        HStack {
+          properties.titleLeftImage?.image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: properties.titleLeftImage?.size.width,
+                   height: properties.titleLeftImage?.size.height)
+          Text(properties.title)
+            .font(properties.font)
+          properties.titleRightImage?.image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: properties.titleRightImage?.size.width,
+                   height: properties.titleRightImage?.size.height)
+        }
+        Spacer()
+        properties.rightImage?.image
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width: properties.rightImage?.size.width,
+                 height: properties.rightImage?.size.height)
       }
+      .padding()
+      .background(backgroundView)
+      .foregroundColor(properties.textColor)
+      .cornerRadius(getCornerRadius(for: properties.cornerRadius))
+      .overlay(
+        RoundedRectangle(cornerRadius: getCornerRadius(for: properties.cornerRadius))
+          .stroke(properties.borderColor, lineWidth: properties.borderWidth)
+      )
+      
     }
-  }
-  
-  public mutating func setAction(action: @escaping () -> Void) {
-    actionHandler = action
+    .buttonStyle(.plain)
   }
 }
 
-// MARK:- Builder Pattern Methods
-public extension ActionButton {
+// MARK: - Public Properties
+extension ActionButton {
+  struct ExtraImage {
+    let image: Image
+    let size: CGSize
+  }
+  
+  enum CornerRadiusType {
+    case rounded
+    case custom(CGFloat)
+  }
+}
+
+// MARK: - Builder Pattern Methods
+extension ActionButton {
   func title(_ title: String) -> ActionButton {
     self.properties.title = title
     return self
@@ -83,14 +134,29 @@ public extension ActionButton {
     return self
   }
   
-  func extraImage(_ extraImage: ActionButton.ExtraImage) -> ActionButton {
-    self.properties.extraImage = extraImage
+  func extraImage(leftImage: ActionButton.ExtraImage) -> ActionButton {
+    self.properties.leftImage = leftImage
+    return self
+  }
+  
+  func extraImage(rightImage: ActionButton.ExtraImage) -> ActionButton {
+    self.properties.rightImage = rightImage
+    return self
+  }
+  
+  func extraImage(titleLeftImage: ActionButton.ExtraImage) -> ActionButton {
+    self.properties.titleLeftImage = titleLeftImage
+    return self
+  }
+  
+  func extraImage(titleRightImage: ActionButton.ExtraImage) -> ActionButton {
+    self.properties.titleRightImage = titleRightImage
     return self
   }
 }
 
 // MARK: - Access to properties from UIKit
-public extension ActionButton {
+extension ActionButton {
   var title: String {
     get { properties.title }
     set { properties.title = newValue}
@@ -126,131 +192,59 @@ public extension ActionButton {
     set { properties.borderWidth = newValue}
   }
   
-  var extraImage: ActionButton.ExtraImage? {
-    get { properties.extraImage }
-    set { properties.extraImage = newValue}
-  }
-}
-
-// MARK: - Public Properties
-public extension ActionButton {
-  enum ExtraImage {
-    case left(Image)
-    case center(Image)
-    case right(Image)
+  var leftImage: ActionButton.ExtraImage? {
+    get { properties.leftImage }
+    set { properties.leftImage = newValue}
   }
   
-  enum CornerRadiusType {
-    case rounded
-    case custom(CGFloat)
+  var rightImage: ActionButton.ExtraImage? {
+    get { properties.rightImage }
+    set { properties.rightImage = newValue}
+  }
+  
+  var titleLeftImage: ActionButton.ExtraImage? {
+    get { properties.titleLeftImage }
+    set { properties.titleLeftImage = newValue}
+  }
+  
+  var titleRightImage: ActionButton.ExtraImage? {
+    get { properties.titleRightImage }
+    set { properties.titleRightImage = newValue}
+  }
+  
+  mutating func addAction(_ action: @escaping () -> Void) {
+    properties.actionHandler = action
   }
 }
 
-// MARK: - Private Methods
+// MARK: - Helper Methods
 private extension ActionButton {
-  func buttonAction() {
-    actionHandler?()
+  @ViewBuilder
+  private var backgroundView: some View {
+    switch properties.backgroundType {
+    case .plain(let backgroundColor):
+      backgroundColor
+    case let .linearGradient(gradient):
+      gradient
+    case let .angularGradient(gradient):
+      gradient
+    case let .radialGradient(gradient):
+      gradient
+    }
   }
   
-  func getAlignment(for extraImage: ExtraImage?) -> Alignment {
-    switch extraImage {
-    case .some(let alignment):
-      switch alignment {
-      case .right:
-        return .trailing
-      case .left:
-        return .leading
-      case .center:
-        return .center
-      }
-    case .none:
-      return .trailing
+  private func getCornerRadius(for cornerType: CornerRadiusType) -> CGFloat {
+    switch cornerType {
+    case .rounded:
+      return .infinity
+    case .custom(let cornerRadius):
+      return cornerRadius
     }
   }
 }
 
-// MARK: Views
-private extension ActionButton {
-  struct ButtonView: View {
-    var size: CGSize
-    var action: () -> Void
-    var properties: ActionButtonViewModel
-    
-    var body: some View {
-      Button(action: {
-        action()
-      }) {
-        Text(properties.title)
-          .font(properties.font)
-          .padding()
-          .frame(width: size.width, height: size.height)
-          .foregroundColor(properties.textColor)
-          .overlay(
-            RoundedRectangle(cornerRadius: getCornerRadius(for: properties.cornerRadius))
-              .stroke(properties.borderColor, lineWidth: properties.borderWidth)
-          )
-      }
-      .background(backgroundView)
-      .cornerRadius(getCornerRadius(for: properties.cornerRadius))
-    }
-    
-    @ViewBuilder
-    var backgroundView: some View {
-      switch properties.backgroundType {
-      case .plain(let backgroundColor):
-        backgroundColor
-      case let .linearGradient(gradient):
-        gradient
-      case let .angularGradient(gradient):
-        gradient
-      case let .radialGradient(gradient):
-        gradient
-      }
-    }
-    
-    func getCornerRadius(for cornerType: CornerRadiusType) -> CGFloat {
-      switch cornerType {
-      case .rounded:
-        return .infinity
-      case .custom(let cornerRadius):
-        return cornerRadius
-      }
-    }
-  }
-  
-  struct ExtraImageView: View {
-    var tintColor: Color
-    var image: Image
-    var height: CGFloat
-    var width: CGFloat
-    
-    init?(imageAlignment: ExtraImage?, tintColor: Color, size: CGSize) {
-      switch imageAlignment {
-      case .some(let alignment):
-        switch alignment {
-        case .center(let image), .left(let image), .right(let image):
-          self.image = image
-        }
-        self.tintColor = tintColor
-        height = size.height * 0.4
-        width = height
-      case .none:
-        return nil
-      }
-    }
-    
-    var body: some View {
-      image
-        .frame(width: width, height: height)
-        .padding()
-        .foregroundColor(tintColor)
-    }
-  }
-}
-
-struct PovioButton_Previews: PreviewProvider {
+struct ActionButton_Previews: PreviewProvider {
   static var previews: some View {
     ActionButton()
   }
 }
-
