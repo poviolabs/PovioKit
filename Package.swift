81ad001220d4d06508fd49b29842574384812f79
Toni K. Turk <kocjanturk@gmail.com>
2023-03-06 10:30:13 +0100

    Removed auth subpacackages

--- Package.swift
+++ Package.swift
@@ -11,15 +11,9 @@ let package = Package(
     .library(name: "PovioKitNetworking", targets: ["PovioKitNetworking"]),
     .library(name: "PovioKitPromise", targets: ["PovioKitPromise"]),
     .library(name: "PovioKitUI", targets: ["PovioKitUI"]),
-    .library(name: "PovioKitAuthCore", targets: ["PovioKitAuthCore"]),
-    .library(name: "PovioKitAuthApple", targets: ["PovioKitAuthApple"]),
-    .library(name: "PovioKitAuthGoogle", targets: ["PovioKitAuthGoogle"]),
-    .library(name: "PovioKitAuthFacebook", targets: ["PovioKitAuthFacebook"]),
   ],
   dependencies: [
     .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: "5.6.4")),
-    .package(url: "https://github.com/google/GoogleSignIn-iOS", .upToNextMajor(from: "7.0.0")),
-    .package(url: "https://github.com/facebook/facebook-ios-sdk", .upToNextMajor(from: "15.1.0")),
   ],
   targets: [
     .target(
@@ -47,36 +41,6 @@ let package = Package(
       ],
       path: "Sources/UI"
     ),
-    .target(
-      name: "PovioKitAuthCore",
-      dependencies: [
-        "PovioKitPromise"
-      ],
-      path: "Sources/Auth/Core"
-    ),
-    .target(
-      name: "PovioKitAuthApple",
-      dependencies: [
-        "PovioKitAuthCore"
-      ],
-      path: "Sources/Auth/Apple"
-    ),
-    .target(
-      name: "PovioKitAuthGoogle",
-      dependencies: [
-        "PovioKitAuthCore",
-        .product(name: "GoogleSignInSwift", package: "GoogleSignIn-iOS")
-      ],
-      path: "Sources/Auth/Google"
-    ),
-    .target(
-      name: "PovioKitAuthFacebook",
-      dependencies: [
-        "PovioKitAuthCore",
-        .product(name: "FacebookLogin", package: "facebook-ios-sdk")
-      ],
-      path: "Sources/Auth/Facebook"
-    ),
     .testTarget(
       name: "Tests",
       dependencies: [
@@ -84,7 +48,6 @@ let package = Package(
         "PovioKitPromise",
         "PovioKitNetworking",
         "PovioKitUI",
-        "PovioKitAuthCore"
       ]
     ),
   ],
