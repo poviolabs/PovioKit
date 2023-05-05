2e7fc18c83644bd6338a31fb928b4818e29497f3
Marko Mijatović <95407653+markom-povio@users.noreply.github.com>
2023-05-05 15:22:34 +0200

    Feature: [Core] EXIF reader & writer (#264)
    Co-authored-by: Borut Tomazin <borut@povio.com>

---
+++ Tests/Tests/Core/Utilities/Exif/ExifTests.swift
@@ -0,0 +1,60 @@
+//
+//  ExifTests.swift
+//  PovioKit_Tests
+//
+//  Created by Borut Tomazin on 05/05/2023.
+//  Copyright © 2023 Povio Inc. All rights reserved.
+//
+
+import XCTest
+import PovioKitCore
+
+final class ExifTests: XCTestCase {
+  let image = UIImage.with(color: .blue, size: .init(width: 100, height: 100))
+  
+  func test_imageExifRead() {
+    guard let imageData = image.jpegData(compressionQuality: 1) else {
+      XCTFail("Failed to generate image data!")
+      return
+    }
+    
+    do {
+      let manager = Exif(source: .data(imageData))
+      let data = try manager.read()
+      XCTAssertFalse(data.isEmpty)
+      XCTAssertTrue(data.contains(where: { $0.key == "{Exif}" }))
+    } catch {
+      XCTFail("Failed to read exif data! \(error.localizedDescription)")
+    }
+  }
+  
+  func test_imageExifWrite() {
+    guard let imageData = image.jpegData(compressionQuality: 1) else {
+      XCTFail("Failed to generate image data!")
+      return
+    }
+    
+    do {
+      let writeManager = Exif(source: .data(imageData))
+      let updateValue = "Test comment!"
+      let updateData = [kCGImagePropertyExifUserComment: updateValue]
+      let updatedData = try writeManager.update(updateData)
+      
+      let readManager = Exif(source: .data(updatedData))
+      let newData = try readManager.read()
+      
+      XCTAssertFalse(newData.isEmpty)
+      guard let exifData = newData["{Exif}"] as? [String: Any] else {
+        XCTFail("Missing exif container!")
+        return
+      }
+      guard let value = exifData["UserComment"] as? String else {
+        XCTFail("Missing UserComment container!")
+        return
+      }
+      XCTAssertEqual(value, updateValue)
+    } catch {
+      XCTFail("Failed to write and/or read exif data! \(error.localizedDescription)")
+    }
+  }
+}
