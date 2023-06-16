fb85d64bf6defced81dd4b9b428c7106390aca20
Ndriqim Nagavci <ndriqim.nagavci@povio.com>
2023-06-16 12:13:08 +0200

    Public isCameraAvailable for DeviceType.

--- Sources/UI/Camera/Camera.swift
+++ Sources/UI/Camera/Camera.swift
@@ -13,9 +13,9 @@ public class Camera: NSObject {
   var device: AVCaptureDevice? {
     switch cameraPosition {
     case .back:
-      return isCameraAvailable(position: .back) ? AVCaptureDevice.default(for: .video) : nil
+      return isCameraAvailable(for: deviceType, position: .back) ? AVCaptureDevice.default(deviceType, for: .video, position: .back) : nil
     case .front:
-      return isCameraAvailable(position: .front) ? AVCaptureDevice.default(deviceType, for: .video, position: .front) : nil
+      return isCameraAvailable(for: deviceType, position: .front) ? AVCaptureDevice.default(deviceType, for: .video, position: .front) : nil
     }
   }
   let session = AVCaptureSession()
@@ -83,12 +83,10 @@ public extension Camera {
     }
     device.unlockForConfiguration()
   }
-}
-
-// MARK: - Private Methods
-private extension Camera {
+  
   /// Check if camera is available on device
-  func isCameraAvailable(position: Camera.CameraPosition) -> Bool {
+  func isCameraAvailable(for deviceType: AVCaptureDevice.DeviceType,
+                         position: Camera.CameraPosition) -> Bool {
     !AVCaptureDevice
       .DiscoverySession(
         deviceTypes: [deviceType],
@@ -97,7 +95,10 @@ private extension Camera {
       .devices
       .isEmpty
   }
-  
+}
+
+// MARK: - Private Methods
+private extension Camera {
   func setTorch(on: Bool) throws {
     guard let device = device, device.hasTorch, device.isTorchAvailable else { return }
     try device.lockForConfiguration()
