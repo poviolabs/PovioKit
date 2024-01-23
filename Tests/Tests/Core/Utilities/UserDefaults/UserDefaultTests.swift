ba0572b1f5e1108a80556d8dc67007172ce19eee
Borut Tomazin <borut@povio.com>
2024-01-23 08:09:46 +0100

    Broken.

--- Tests/Tests/Core/Utilities/UserDefaults/UserDefaultTests.swift
+++ Tests/Tests/Core/Utilities/UserDefaults/UserDefaultTests.swift
@@ -16,7 +16,6 @@ class UserDefaultTests: XCTestCase {
     try super.tearDownWithError()
     userDefaults.removeObject(forKey: Defaults.testBoolKey)
     userDefaults.removeObject(forKey: Defaults.testStringKey)
-    userDefaults.synchronize()
   }
   
   func testSaveStringValue() {
@@ -36,17 +35,37 @@ class UserDefaultTests: XCTestCase {
     // Then
     XCTAssertEqual(userDefaults.bool(forKey: Defaults.testBoolKey), Defaults.isAuthenticated)
   }
+  
+  func testSaveDataValue() {
+    let givenValue: Data = .init()
+    
+    Defaults.profileData = givenValue
+    
+    XCTAssertNotNil(Defaults.profileData)
+  }
+  
+  func testSaveDataNullValue() {
+    let givenValue: Data? = nil
+    
+    Defaults.profileData = givenValue
+    
+    XCTAssertNil(Defaults.profileData)
+  }
 }
 
 extension UserDefaultTests {
   struct Defaults {
     static var testBoolKey = "test_bool_key"
     static var testStringKey = "test_string_key"
+    static var testDataKey = "test_data_key"
     
     @UserDefault(defaultValue: false, key: testBoolKey)
     static var isAuthenticated: Bool
     
     @UserDefault(defaultValue: "default", key: testStringKey)
     static var screenName: String
+    
+    @UserDefault(defaultValue: nil, key: testDataKey)
+    static var profileData: Data?
   }
 }
