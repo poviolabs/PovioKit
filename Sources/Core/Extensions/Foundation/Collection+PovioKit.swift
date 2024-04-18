531627c7e1deea38f2a3d511c7a9c90786f8cd70
Toni K. Turk <kocjanturk@gmail.com>
2024-04-18 10:30:53 +0200

    Updated implementation

--- Sources/Core/Extensions/Foundation/Collection+PovioKit.swift
+++ Sources/Core/Extensions/Foundation/Collection+PovioKit.swift
@@ -16,15 +16,18 @@ public extension Collection where Indices.Iterator.Element == Index {
 }
 
 public extension Collection {
-  /// Conditional element count - https://github.com/apple/swift-evolution/blob/master/proposals/0220-count-where.md
-  func count(where predicate: (Element) throws -> Bool) rethrows -> Int {
-    var count = 0
-    for el in self {
-      if try predicate(el) {
-        count += 1
+  /// Conditional element count - https://forums.swift.org/t/refresh-review-se-0220-count-where/66235/4
+  @inlinable
+  func count(
+    where predicate: (Element) throws -> Bool
+  ) rethrows -> Int {
+    try reduce(0) { n, element in
+      if try predicate(element) { 
+        n + 1 
+      } else { 
+        n 
       }
     }
-    return count
   }
 }
 
