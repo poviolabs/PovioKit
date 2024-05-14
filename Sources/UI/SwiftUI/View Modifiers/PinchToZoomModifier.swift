83660af95733148bd264461dd6f9933a7297b912
Yll Fejziu <yllfejziu@gmail.com>
2024-05-14 15:59:19 +0200

    Utilize ViewModel binding instead of delegate pattern

--- Sources/UI/SwiftUI/View Modifiers/PinchToZoomModifier.swift
+++ Sources/UI/SwiftUI/View Modifiers/PinchToZoomModifier.swift
@@ -9,28 +9,22 @@
 import SwiftUI
 
 /// https://stackoverflow.com/a/59878898
+@available(iOS 15.0, *)
 public struct PinchToZoomModifier: ViewModifier {
-  @State var scale: CGFloat = 1.0
-  @State var anchor: UnitPoint = .center
-  @State var offset: CGSize = .zero
-  @State var isPinching: Bool = false
+  @StateObject private var viewModel = PinchZoomViewModel()
 
   public func body(content: Content) -> some View {
     content
-      .scaleEffect(scale, anchor: anchor)
-      .offset(offset)
-      .animation(isPinching ? .none : .spring(), value: scale)
+      .scaleEffect(viewModel.scale, anchor: viewModel.anchor)
+      .offset(viewModel.offset)
+      .animation(viewModel.isPinching ? .none : .spring(), value: viewModel.scale)
       .overlay(
-        PinchZoom(
-          scale: $scale,
-          anchor: $anchor,
-          offset: $offset,
-          isPinching: $isPinching
-        )
+        PinchZoom(viewModel: viewModel)
       )
   }
 }
 
+@available(iOS 15.0, *)
 public extension View {
   func pinchToZoom() -> some View {
     self.modifier(PinchToZoomModifier())
@@ -38,45 +32,21 @@ public extension View {
 }
 
 // MARK: - Private
-private protocol PinchZoomViewDelgate: AnyObject {
-  func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangePinching isPinching: Bool)
-  func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeScale scale: CGFloat)
-  func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeAnchor anchor: UnitPoint)
-  func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeOffset offset: CGSize)
+private class PinchZoomViewModel: ObservableObject {
+  @Published var scale: CGFloat = 1.0
+  @Published var anchor: UnitPoint = .center
+  @Published var offset: CGSize = .zero
+  @Published var isPinching: Bool = false
+  var startLocation: CGPoint = .zero
+  var location: CGPoint = .zero
+  var numberOfTouches: Int = 0
 }
 
 private class PinchZoomView: UIView {
-  weak var delegate: PinchZoomViewDelgate?
+  var viewModel: PinchZoomViewModel
 
-  private(set) var scale: CGFloat = 0 {
-    didSet {
-      delegate?.pinchZoomView(self, didChangeScale: scale)
-    }
-  }
-
-  private(set) var anchor: UnitPoint = .center {
-    didSet {
-      delegate?.pinchZoomView(self, didChangeAnchor: anchor)
-    }
-  }
-
-  private(set) var offset: CGSize = .zero {
-    didSet {
-      delegate?.pinchZoomView(self, didChangeOffset: offset)
-    }
-  }
-
-  private(set) var isPinching: Bool = false {
-    didSet {
-      delegate?.pinchZoomView(self, didChangePinching: isPinching)
-    }
-  }
-
-  private var startLocation: CGPoint = .zero
-  private var location: CGPoint = .zero
-  private var numberOfTouches: Int = 0
-
-  init() {
+  init(viewModel: PinchZoomViewModel) {
+    self.viewModel = viewModel
     super.init(frame: .zero)
 
     let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(pinch(gesture:)))
@@ -84,36 +54,42 @@ private class PinchZoomView: UIView {
     addGestureRecognizer(pinchGesture)
   }
 
+  @available(*, unavailable)
   required init?(coder: NSCoder) {
-    super.init(coder: coder)
+    fatalError("init(coder:) has not been implemented")
   }
 
   @objc private func pinch(gesture: UIPinchGestureRecognizer) {
     switch gesture.state {
     case .began:
-      isPinching = true
-      startLocation = gesture.location(in: self)
-      anchor = UnitPoint(x: startLocation.x / bounds.width, y: startLocation.y / bounds.height)
-      numberOfTouches = gesture.numberOfTouches
+      viewModel.isPinching = true
+      viewModel.startLocation = gesture.location(in: self)
+      viewModel.anchor = UnitPoint(x: viewModel.startLocation.x / bounds.width,
+                                   y: viewModel.startLocation.y / bounds.height)
+      viewModel.numberOfTouches = gesture.numberOfTouches
     case .changed:
-      if gesture.numberOfTouches != numberOfTouches {
+      if gesture.numberOfTouches != viewModel.numberOfTouches {
         // If the number of fingers being used changes, the start location needs to be adjusted to avoid jumping.
         let newLocation = gesture.location(in: self)
-        let jumpDifference = CGSize(width: newLocation.x - location.x, height: newLocation.y - location.y)
-        startLocation = CGPoint(x: startLocation.x + jumpDifference.width, y: startLocation.y + jumpDifference.height)
-
-        numberOfTouches = gesture.numberOfTouches
+        let jumpDifference = CGSize(width: newLocation.x - viewModel.location.x,
+                                    height: newLocation.y - viewModel.location.y)
+        viewModel.startLocation = CGPoint(
+          x: viewModel.startLocation.x + jumpDifference.width,
+          y: viewModel.startLocation.y + jumpDifference.height
+        )
+        viewModel.numberOfTouches = gesture.numberOfTouches
       }
-
-      scale = gesture.scale
-
-      location = gesture.location(in: self)
-      offset = CGSize(width: location.x - startLocation.x, height: location.y - startLocation.y)
+      viewModel.scale = gesture.scale
+      viewModel.location = gesture.location(in: self)
+      viewModel.offset = CGSize(
+        width: viewModel.location.x - viewModel.startLocation.x,
+        height: viewModel.location.y - viewModel.startLocation.y
+      )
     case .ended, .cancelled, .failed:
-      isPinching = false
-      scale = 1.0
-      anchor = .center
-      offset = .zero
+      viewModel.isPinching = false
+      viewModel.scale = 1.0
+      viewModel.anchor = .center
+      viewModel.offset = .zero
     default:
       break
     }
@@ -121,44 +97,13 @@ private class PinchZoomView: UIView {
 }
 
 private struct PinchZoom: UIViewRepresentable {
-  @Binding var scale: CGFloat
-  @Binding var anchor: UnitPoint
-  @Binding var offset: CGSize
-  @Binding var isPinching: Bool
-
-  func makeCoordinator() -> Coordinator {
-    Coordinator(self)
-  }
+  @ObservedObject var viewModel: PinchZoomViewModel
 
   func makeUIView(context: Context) -> PinchZoomView {
-    let pinchZoomView = PinchZoomView()
-    pinchZoomView.delegate = context.coordinator
-    return pinchZoomView
+    .init(viewModel: viewModel)
   }
 
-  func updateUIView(_ pageControl: PinchZoomView, context: Context) { /* nothing to do here */ }
-
-  class Coordinator: NSObject, PinchZoomViewDelgate {
-    var pinchZoom: PinchZoom
-
-    init(_ pinchZoom: PinchZoom) {
-      self.pinchZoom = pinchZoom
-    }
-
-    func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangePinching isPinching: Bool) {
-      pinchZoom.isPinching = isPinching
-    }
-
-    func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeScale scale: CGFloat) {
-      pinchZoom.scale = scale
-    }
-
-    func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeAnchor anchor: UnitPoint) {
-      pinchZoom.anchor = anchor
-    }
-
-    func pinchZoomView(_ pinchZoomView: PinchZoomView, didChangeOffset offset: CGSize) {
-      pinchZoom.offset = offset
-    }
+  func updateUIView(_ uiView: PinchZoomView, context: Context) {
+    // state is automatically updated via ViewModel binding
   }
 }
