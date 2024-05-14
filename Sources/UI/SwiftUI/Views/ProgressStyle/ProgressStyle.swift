272f5c903021ff9a2a366c3896cb1f05c4aa58af
Borut Tomazin <borut@povio.com>
2024-05-14 08:43:07 +0200

    Feature/SwiftUI views (#308)

---
+++ Sources/UI/SwiftUI/Views/ProgressStyle/ProgressStyle.swift
@@ -0,0 +1,38 @@
+//
+//  ProgressStyle.swift
+//  PovioKit
+//
+//  Created by Borut Tomazin on 02/03/2024.
+//  Copyright © 2024 Povio Inc. All rights reserved.
+//
+
+
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct ProgressStyle: ProgressViewStyle {
+  var trackColor: Color = .gray
+  var progressColor: Color = .black
+  var cornerRadius: CGFloat = 8
+  
+  public func makeBody(configuration: Configuration) -> some View {
+    let completed = configuration.fractionCompleted ?? 0
+    return GeometryReader { geo in
+      ZStack(alignment: .leading) {
+        RoundedRectangle(cornerRadius: cornerRadius)
+          .fill(trackColor)
+        RoundedRectangle(cornerRadius: cornerRadius)
+          .fill(progressColor)
+          .frame(width: completed * geo.size.width)
+      }
+    }
+  }
+}
+
+@available(iOS 15.0, *)
+#Preview {
+  ProgressView(value: 0.5)
+    .progressViewStyle(ProgressStyle())
+    .frame(height: 5)
+    .padding(.horizontal, 20)
+}
