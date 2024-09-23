343c73f0ff62dd504ca011d447729df353795f74
Ndriqim Nagavci <ndriqim.nagavci@povio.com>
2024-09-23 10:16:15 +0200

    Introduce PhotoPreviewImageLoader.

---
+++ Sources/UI/SwiftUI/Views/PhotoPreview/PhotoPreviewImageLoader.swift
@@ -0,0 +1,42 @@
+//
+//  PhotoPreviewImageLoader.swift
+//  PovioKit
+//
+//  Created by Ndriqim Nagavci on 23/09/2024.
+//  Copyright © 2024 Povio Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@MainActor
+class PhotoPreviewImageLoader: ObservableObject {
+  @Published private(set) var state: State = .loading
+  
+  func loadImage(from url: URL?) async {
+    guard let url else { return }
+    let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
+    do {
+      let (data, _) = try await URLSession.shared.data(for: request)
+      await getImageFrom(data: data)
+    } catch {
+      state = .failed
+    }
+  }
+  
+  func getImageFrom(data: Data) async {
+    guard let loadedImage = UIImage(data: data) else {
+      state = .failed
+      return
+    }
+    state = .loaded(loadedImage)
+  }
+}
+
+// MARK: - State
+extension PhotoPreviewImageLoader {
+  enum State {
+    case loading
+    case loaded(UIImage)
+    case failed
+  }
+}
