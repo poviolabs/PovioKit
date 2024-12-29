944c1773435521a0f55ea6caf82aaff1d22625e1
Borut Tomazin <borut@povio.com>
2024-12-29 17:11:03 +0100

    Cleanup.

--- Sources/UI/SwiftUI/Extensions/Text+PovioKit.swift
+++ Sources/UI/SwiftUI/Extensions/Text+PovioKit.swift
@@ -10,7 +10,7 @@ import PovioKitCore
 import SwiftUI
 
 public extension Text {
-  /// Creates an instance of hyperlinked Text using markdown formatting syntac
+  /// Creates an instance of hyperlinked Text using markdown formatting syntax
   /// For dynamic strings, the link string has to be converted into an AttributedString
   /// https://developer.apple.com/videos/play/wwdc2021/10018/?time=1638
   @available(iOS 15.0, *)
