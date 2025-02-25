9f2976177375cd800fc20ee9190e32fe61eba98b
Borut Tomazin <borut@povio.com>
2025-02-25 14:56:09 +0100

    Access.

--- Sources/UI/SwiftUI/Views/AnimatedImage/AnimatedImage.swift
+++ Sources/UI/SwiftUI/Views/AnimatedImage/AnimatedImage.swift
@@ -12,14 +12,22 @@ import SwiftUI
 /// A view that displays an animated image either from a local file or a remote URL.
 ///
 /// It uses `Kingfisher` for handling the image loading from both sources.
-struct AnimatedImage: View {
-  let source: Source
+@available(iOS 15.0, *)
+public struct AnimatedImage: View {
+  private let source: Source
+  private let animated: Bool
   
-  var body: some View {
+  public init(source: Source, animated: Bool = false) {
+    self.source = source
+    self.animated = animated
+  }
+  
+  public var body: some View {
     switch source {
     case .local(let fileName):
       if let fileUrl = Bundle.main.url(forResource: fileName, withExtension: "gif") {
         KFAnimatedImage(source: .provider(LocalFileImageDataProvider(fileURL: fileUrl)))
+          .fade(duration: animated ? 0.25 : 0)
       }
     case .remote(let url):
       KFAnimatedImage(url)
@@ -27,11 +35,12 @@ struct AnimatedImage: View {
   }
 }
 
-extension AnimatedImage {
+@available(iOS 15.0, *)
+public extension AnimatedImage {
   /// Enum representing the source of the animated image.
   enum Source {
     case local(fileName: String)
-    case remote(url: URL)
+    case remote(url: URL?)
   }
 }
 #endif
